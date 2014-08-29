e5927af727f8f11a656745df39853251803b7387
hoshikoo <hoshiko@hoshikos-mini.home>
2014-08-29 18:27:31 -0400

    082914

--- app.js
+++ app.js
@@ -1,32 +1,36 @@
 $(document).ready(function(){
 
+	    var numberInput = undefined;
+		var i = undefined;
+		var fizzBuzz =function(){
+		    while(i % 1!=0){
+		    	numberInput = prompt("Enter any whole number");
+				i = + "numberInput";
+				if(i % 1!=0){
+				alert(i + " is not a whole number.  Please enter a whole number.");	
+				}
+		    }
+			
+			for (i = 1; i <= numberInput; i++) {
+		    	if(i%15 === 0){
+		    	$("<p>FizzBuzz</p>").appendTo("#result");
+		    	}
+		    	else if(i%3 === 0){
+		        $("<p>Fizz</p>").appendTo("#result");
+		    	}
+		    	else if(i%5 === 0){
+		        $("<p>Buzz</p>").appendTo("#result");
+		        }
+		    	else{
+		        $("<p>"+i+"</p>").appendTo("#result");
+		        }
+			};	
+		}
 	
 	$("#start").click(function(e){
 		e.preventDefault();
 	    $("p").remove();
-	    var numberInput = undefined;
-		var i = undefined;
-	    while(isNaN(i)){
-	    	numberInput = prompt("Enter any number");
-			i = numberInput;
-			if(isNaN(i)){
-			alert(i + " is not a number.  Please enter a number.");	
-			}
-	    }
-		
-		for (i = 1; i <= numberInput; i++) {
-	    	if(i%15 === 0){
-	    	$("<p>FizzBuzz</p>").appendTo("#result");
-	    	}
-	    	else if(i%3 === 0){
-	        $("<p>Fizz</p>").appendTo("#result");
-	    	}
-	    	else if(i%5 === 0){
-	        $("<p>Buzz</p>").appendTo("#result");
-	        }
-	    	else{
-	        $("<p>"+i+"</p>").appendTo("#result");
-	        }
+	    fizzBuzz;
 		}	
 	});
 
